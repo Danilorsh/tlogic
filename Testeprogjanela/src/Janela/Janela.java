c3fe06c8723f0947e8286f65bdd508acae1c420d
Danilorsh <danilorsh@hotmail.com>
2018-08-20 22:02:32 -0300

    Janela Frame

--- Testeprogjanela/src/Janela/Janela.java
+++ Testeprogjanela/src/Janela/Janela.java
@@ -3,6 +3,7 @@ package Janela;
 import javax.swing.JFrame;	
 	public class Janela {
     public static void main(String[] args) {
+    	
 	JFrame janela = new JFrame("Minha Primeira janela");
 	janela.setVisible(true);
     }
