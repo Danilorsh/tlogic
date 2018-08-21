76b8005d9d5c9ee0309d1103b4c7f89807467ab5
Danilorsh <danilorsh@hotmail.com>
2018-08-20 22:39:49 -0300

    classe operadores

---
+++ Operadores/src/Operador/Operadores.java
@@ -0,0 +1,19 @@
+package Operador;
+public class Operadores {
+	public static void main(String args[]){
+		int numero = 10 , numero2 = 5, total = 0;
+		total  = numero * numero2;
+		System.out.println(total);
+		
+		if (total <=100) {
+			System.out.println("Voce Perdeu!!!");	
+		}else if(total >100) {
+			System.out.println("Voce ganhou!!");
+		
+			
+			
+		}
+	
+	
+	}
+}
