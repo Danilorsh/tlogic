d54a48a64d04075b016ce7d647a8431700bb5e8b
Danilorsh <danilorsh@hotmail.com>
2018-08-20 21:51:38 -0300

    Janela

---
+++ Testeprogjanela/src/Janela/Janela.java
@@ -0,0 +1,9 @@
+package Janela;
+	
+import javax.swing.JFrame;	
+	public class Janela {
+    public static void main(String[] args) {
+	JFrame janela = new JFrame("Minha Primeira janela");
+	janela.setVisible(true);
+    }
+}
